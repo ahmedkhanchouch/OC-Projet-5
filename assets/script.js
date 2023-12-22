945689286ac9528b9002bca50bddaa459324b873
ahmed khanchouch <akhanchouch93@gmail.com>
2023-12-22 14:20:31 +0100

    fonction de flesh

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+let imageCourante = 0;
+const listImages = ['/assets/images/slideshow/slide1.jpg', '/assets/images/slideshow/slide2.jpg',	'/assets/images/slideshow/slide3.jpg' , '/assets/images/slideshow/slide4.png'];
+
+document.getElementById('flecheDroite').addEventListener('click', () => {
+  imageCourante = imageCourante + 1;
+  document
+    .getElementById('img-carroussel')
+    .setAttribute('src', listImages[imageCourante]);
+});
+
+document.getElementById('flecheGauche').addEventListener('click', () => {
+  imageCourante = imageCourante - 1;
+  document
+    .getElementById('img-carroussel')
+    .setAttribute('src', listImages[imageCourante]);
+	
+});
+
 const slides = [
 	{
 		"image":"slide1.jpg",
